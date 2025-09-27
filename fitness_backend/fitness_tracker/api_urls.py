6abfed2ce3f65cd5e4d3cc5dd4d1cd96698e655f
mdey26 <86187805+mdey26@users.noreply.github.com>
2025-09-27 13:52:13 +0530

    Delete fitness_backend directory

--- fitness_backend/fitness_tracker/api_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('auth/', include('accounts.api_urls')),
-    path('workouts/', include('workouts.api_urls')), 
-    path('nutrition/', include('nutrition.api_urls')),
-    path('goals/', include('goals.api_urls')),
-    path('community/', include('community.api_urls')),
-] 
--- fitness_backend/goals/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Goals endpoints will be added here later
-]
